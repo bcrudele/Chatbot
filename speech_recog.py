234e3217d449b4e594b66c05024375242c80c353
bcrudele <bcrudele@purdue.edu>
2023-10-16 02:01:00 -0400

    Command re-prompting

--- speech_recog.py
+++ speech_recog.py
@@ -1,21 +1,26 @@
 import speech_recognition as sr
 # https://pypi.org/project/SpeechRecognition/
 
-delay = 2  # timeout var in listen()
+delay = 1.5  # timeout var in listen()
 
 def user_listen():
     recognizer = sr.Recognizer()
+    while True:
+        with sr.Microphone() as source:
+            print("Start Speaking...")
+            recognizer.adjust_for_ambient_noise(source)
 
-    with sr.Microphone() as source:
-        print("Start Speaking...")
-        recognizer.adjust_for_ambient_noise(source)
+            # timeout: the maximum time to wait for audio before timing out
+            # phrase_time_limit: the maximum time allowed for speaking a phrase
+            try:
+                audio = recognizer.listen(source, timeout=delay)
+            except sr.WaitTimeoutError:
+                print("Try speaking, couldn't hear you:\n")
+                continue
 
-        # timeout: the maximum time to wait for audio before timing out
-        # phrase_time_limit: the maximum time allowed for speaking a phrase
-        audio = recognizer.listen(source, timeout=delay)
-
-    try:
-        return recognizer.recognize_google(audio)
-    except sr.RequestError:
-        print("Unable to access the Google API")
-        return None
+        try:
+            return recognizer.recognize_google(audio)
+        except sr.RequestError:
+            print("Unable to access the Google API")
+            return None
+        

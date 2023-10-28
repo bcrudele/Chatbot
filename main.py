9a907ad6349aa0c2c59bd9102f273dbceb72dea6
bcrudele <bcrudele@purdue.edu>
2023-10-27 21:43:32 -0400

    Weather Scrape Implementation to Main

--- main.py
+++ main.py
@@ -2,16 +2,29 @@ import google_tts   # Has Google TTS
 import gpt_import   # Has ChatGPT 
 import speech_recog # Has Speech Recognition
 import time         # For runtime
+import weather_scrape as ws # For Weather
+from googlesearch import search
+import requests
+from bs4 import BeautifulSoup
 
 iter = 0            # Keeps track of iterations
-program_stoppage = 'stop program'
+program_stop_command_prompt = 'hippopotamus'
+web_prompt = 'search'
+weather_prompt = 'weather'
 
 if __name__ == "__main__":
     while True:
-        command = speech_recog.user_listen()                    # Gets user input
-        if command == program_stoppage:
+        command = speech_recog.user_listen() # Gets user input
+        print(command)
+        if program_stop_command_prompt in command:
             print('Program stopped.')
             break
+        if weather_prompt in command:
+            print("Searching weather data...")
+            city = 'West-Lafayette'    # add UI later
+            state = 'IN'               # add UI later
+            data = ws.weather_scrape(city, state)
+            print(data)         # returns data in the form [temp, outlook, range, wind speed]
         else:
             start = time.time()
             result = gpt_import.communicate_with_openai(command)    # Sends user input to ChatGPT
